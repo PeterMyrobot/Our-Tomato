4d60e9dea0e19e30b90bb6f2368bf0e40c523b2c
Peter Yang <peter.yang@actura.com.tw>
2020-04-01 18:59:44 +0800

    minor update

--- ourTomato.py
+++ ourTomato.py
@@ -16,18 +16,20 @@ BG_COLOR = (50, 74, 92)
 EMPTY_COLOR = (99, 100, 101)
 FILL_COLOR = (172, 222, 216)
 
+MINS_RECT = (10, 10, 30, 20)
+SECS_RECT = (52, 10, 30, 20)
+LEFTBUTTON_RECT = (10,300-42,)
+DASH_RECT = (45, 10,15)
+
 win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 clock = pygame.time.Clock()
-MOVEEVENT, t = pygame.USEREVENT+1, 1000
+MOVEEVENT, t = pygame.USEREVENT + 1, 1000
 pygame.time.set_timer(MOVEEVENT, t)
 
-PAUSE_IMG = pygame.image.load(os.path.join("icon", "pause.png"))
-PAUSE_IMG_HOVER = pygame.image.load(os.path.join("icon", "pause_hover.png"))
-PLAY_IMG = pygame.image.load(os.path.join("icon", "play.png"))
-PLAY_IMG_HOVER = pygame.image.load(os.path.join("icon", "play_hover.png"))
-STOP_IMG = pygame.image.load(os.path.join("icon", "stop.png"))
-STOP_IMG_HOVER = pygame.image.load(os.path.join("icon", "stop_hover.png"))
+PAUSE_IMG = [ pygame.image.load(os.path.join("icon", "pause.png")), pygame.image.load(os.path.join("icon", "pause_hover.png"))]
+PLAY_IMG = [pygame.image.load(os.path.join("icon", "play.png")), pygame.image.load(os.path.join("icon", "play_hover.png"))]
+STOP_IMG = [pygame.image.load(os.path.join("icon", "stop.png")), pygame.image.load(os.path.join("icon", "stop_hover.png"))]
 
 
 def show_text(text, x, y, size, color, win):
@@ -35,8 +37,37 @@ def show_text(text, x, y, size, color, win):
     text = my_font.render(text, True, color)
     win.blit(text, (x, y))
 
-def show_text_input( win):
-    round_rect(win,(10,10,30,20), (255,255,255), 3)
+
+def show_text_input(win, work, rest):
+    round_rect(win, (10, 10, 30, 20), (255, 255, 255), 3)
+    round_rect(win, (52, 10, 30, 20), (255, 255, 255), 3)
+
+    show_text('-', 45, 10, 15, (255, 255, 255), win)
+    show_text(str(work), 18, 10, 15, BG_COLOR, win)
+    show_text(str(rest), 60, 10, 15, BG_COLOR, win)
+
+
+work_time = 25
+rest_time = 5
+
+rundown_sec = 25*60
+
+is_left_hover = 0
+is_right_hover = 0
+
+
+def change_time(pos, is_add):
+    global work_time, rest_time
+    if 10 < pos[0] < 40 and 10 < pos[1] < 30:
+        if is_add:
+            work_time += 1
+        else:
+            work_time -= 1
+    elif 52 < pos[0] < 82 and 10 < pos[1] <30:
+        if is_add:
+            rest_time += 1
+        else:
+            rest_time -= 1
 
 def draw_circle(angle, radius, width, x, y, color, win):
     pass
@@ -48,8 +79,6 @@ def draw_circle(angle, radius, width, x, y, color, win):
     #     degree += math.pi/100
 
 
-rundown_sec = 25*60
-
 def convert_to_time(sec):
     mins, secs = divmod(sec, 60)
     timeformat = '{:0>2d}:{:0>2d}'.format(mins, secs)
@@ -57,21 +86,17 @@ def convert_to_time(sec):
 
 
 def render_win(win):
-
     win.fill(BG_COLOR)
 
     show_text(convert_to_time(rundown_sec), 90, 100, 60, (255, 255, 255), win)
-    show_text_input(win)
-    show_text('25', 18, 10, 15, BG_COLOR, win)
+    show_text_input(win, work_time, rest_time)
+
     draw_circle(6.28, 100, 3, 150, 150, EMPTY_COLOR, win)
     draw_circle(3.14, 100, 3, 150, 150, FILL_COLOR, win)
 
-    win.blit(PAUSE_IMG, (20, 250))
-    win.blit(PAUSE_IMG_HOVER, (60, 250))
-    win.blit(PLAY_IMG, (100, 250))
-    win.blit(PLAY_IMG_HOVER, (140, 250))
-    win.blit(STOP_IMG, (180, 250))
-    win.blit(STOP_IMG_HOVER, (220, 250))
+
+    win.blit(PLAY_IMG[is_left_hover], (10, 300-42))
+    win.blit(PAUSE_IMG[is_right_hover], (300-42, 300-42))
     pygame.display.update()
 
 
@@ -82,11 +107,16 @@ while run:
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            print('start count')
+            if event.button == 1:
+                # do button check
+                print("button press?")
+                pass
+            elif event.button == 4:
+                change_time(event.pos, True)
+            elif event.button == 5:
+                change_time(event.pos, False)
         if event.type == MOVEEVENT:
             rundown_sec -= 1
     render_win(win)
-
 
 pygame.quit()
